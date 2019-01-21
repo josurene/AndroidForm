bc838fac3cf862ee1c0f8b75101d315bc43a4664
Unknown <josurenevp@hotmail.com>
2019-01-21 10:54:22 -0800

    ViewTextField added
    for doing abstract work and adding validations to views like imageButton

---
+++ form/src/main/java/com/form/josu/form/fields/baseFields/ViewTextField.java
@@ -0,0 +1,57 @@
+package com.form.josu.form.fields.baseFields;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.form.josu.form.exceptions.WrongViewException;
+import com.form.josu.form.fields.interfaces.Field;
+
+public abstract class ViewTextField implements Field {
+
+    private TextView textView;
+
+    public ViewTextField(View view) {
+        if(!(view instanceof TextView)){
+            throw new WrongViewException(this.getClass().getName());
+        }
+        textView = (EditText)view;
+    }
+
+    @Override
+    public void setView(View view) {
+        if(view instanceof TextView){
+            setTextView((EditText) view);
+        }
+    }
+
+    public void setText(String text){
+        textView.setText(text);
+    }
+
+    @Override
+    public void setError(String error) {
+        textView.setError(error);
+    }
+
+    @Override
+    public View getView() {
+        return getTextView();
+    }
+
+    public TextView getTextView() {
+        return textView;
+    }
+
+    public void setTextView(EditText mtextView){
+        textView = mtextView;
+    }
+
+    public String getText() {
+        return getTextView().getText().toString();
+    }
+
+    public Double getDouble() {
+        return Double.valueOf(getText());
+    }
+}
